1ca149d99ae99a478666d2b24352d441c1dccf25
lee <dlehgus113@naver.com>
2022-05-02 14:39:08 +0900

    3-3. Hooks 3

--- src/components/2-4 Props/Composition.jsx
+++ src/components/2-4 Props/Composition.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-function Welcome(props){
+function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
 
 
 export default function Composition() {
- 
+
     return (
         <div>
-            <Welcome name ="sara"></Welcome>
-            <Welcome name ="tome"></Welcome>
-            <Welcome name ="aaa"></Welcome>
-            
+            <Welcome name="sara"></Welcome>
+            <Welcome name="tome"></Welcome>
+            <Welcome name="aaa"></Welcome>
+
         </div>
-  )
+    )
 
 }
