b8ed3831b0d8dad2dfe814ac0db022610e871265
lee <dlehgus113@naver.com>
2022-05-03 09:38:52 +0900

    3-8. Memoization2

--- src/components/3-8 Memoization/CommentItem.jsx
+++ src/components/3-8 Memoization/CommentItem.jsx
@@ -1,15 +1,43 @@
-import React, {memo} from 'react';
+import React, {Profiler, memo, useState, useMemo} from 'react';
 import "./CommentItem.css";
 
-function CommentItem({title, content, likes}) {
+function CommentItem({title, content, likes, onClick}) {
+  const [clickCount, setClickCount] = useState(0);
+  function onRenderCallback(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions
+  ){
+    console.log(`actualDuration(${title}: ${actualDuration})`);
+  }
+
+  const handleClick = () =>{
+    onClick();
+    setClickCount(prev=> prev+1);
+    alert(`${title} 눌림`);
+  } 
+  const rate = useMemo(() => {
+    return likes > 10 ? 'Good' : 'Bad';
+  }, [likes]);
+
   return (
-    <div className="CommentItem">
+    <Profiler id="CommentItem" onRender={onRenderCallback}>
+    <div className="CommentItem" onClick={handleClick}>
         <span>{title}</span>
         <br />
         <span>{content}</span>
         <br />
         <span>{likes}</span>
+        <br />
+        <span>{rate}</span>
+        <br />
+        <span>{clickCount}</span>
     </div>
+    </Profiler>
   )
 }
 
